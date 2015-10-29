22015540ab2036c66c5f27595f6ec0004c8f64f3
John van de Water <john.vd.water85@gmail.com>
2015-10-29 23:49:34 +0100

    CHG: updated folder structure

---
+++ templates/src/scripts/main.js
@@ -0,0 +1,1 @@
+const $ = document.querySelectorAll.bind(document)
--- templates/tasks/templates.js
+++ templates/tasks/templates.js
@@ -7,7 +7,7 @@ const reload = browserSync.reload;
 
 gulp.task('templates', () => {
 
-  gulp.src('./src/*.jade')
+  gulp.src(['./src/*.jade', './src/**/*.jade'])
     .pipe(plugins.plumber({errorHandler: plugins.notify.onError((error) => error.message)}))
     .pipe(plugins.jade({
       pretty: true
